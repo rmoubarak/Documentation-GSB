616c25e4f6464d78f36a73c5675949e4abefa9e5
Moubarak <ramadanmoubarak33@gmail.com>
2025-04-24 19:21:20 +0200

    Version du 24/04/2025

---
+++ Code source GSB/GSB/FrmConsulterVisite.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GSB
+{
+    public partial class FrmConsulterVisite : FrmBase
+    {
+        public FrmConsulterVisite()
+        {
+            InitializeComponent();
+        }
+
+
+        #region procédure évènementielle
+        private void FrmConsulterVisite_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+
+        #endregion
+    }
+    
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+PPE GSB Documentation concernant le développement de gestion des visites del'entreprise GSB.
+
+Code source GSB. Documentataion technique. Fiche descriptive de réalisation professionnelle. Cahier des charges.
